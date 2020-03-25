d9082043f2b30413ceda3598918e18e19934b3bf
yusu <dbtnss@naver.com>
2020-03-25 10:41:46 +0900

    test

--- js/index.js
+++ js/index.js
@@ -1,11 +1,9 @@
 window.addEventListener("load", function () { //window load후 실행
-    var visual = document.querySelector(".visual"),
+    var visualImgs = document.querySelectorAll(".visual_imgs img"),
         visualImgArrow = document.querySelector(".arrow"),
         visualImgPrev = document.querySelectorAll(".arrow i")[0],
         visualImgNext = document.querySelectorAll(".arrow i")[1],
         visualImgIndex = 0;
-
-    console.log(visual.style);
 
 
 
@@ -23,16 +21,16 @@ window.addEventListener("load", function () { //window load후 실행
         if (e.target == visualImgPrev) { //이전 visualImg
             visualImgIndex--;
             if (visualImgIndex == -1) {
-                visualImgIndex = 2;
+                visualImgIndex = visualImgs.length - 1;
             }
         } else if (e.target == visualImgNext) { //다음 visualImg
             visualImgIndex++;
-            if (visualImgIndex == 3) {
+            if (visualImgIndex == visualImgs.length) {
                 visualImgIndex = 0;
             }
         }
 
-        //visualImg 변경
+        visualImgChange();
 
     }
 
@@ -43,6 +41,14 @@ window.addEventListener("load", function () { //window load후 실행
             visualImgIndex = 0;
         }
 
-        //visualImg 변경
+        visualImgChange();
+    }
+
+    function visualImgChange() {
+        for (var i = 0; i < visualImgs.length; i++) {
+            visualImgs[i].classList.remove("active");
+        }
+
+        visualImgs[visualImgIndex].classList.add("active"); //visualImg 변경
     }
 })
